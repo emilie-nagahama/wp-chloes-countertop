b1702490dc7db2bd444ca58f8ddb0ef57375610f
Emilie Nagahama <emilienagahama@Emilies-MacBook.local>
2017-08-07 13:22:57 -0700

    add video back in

--- wp-content/themes/chloescountertop/page-about.php
+++ wp-content/themes/chloescountertop/page-about.php
@@ -123,6 +123,11 @@ get_header(); ?>
 </div>
 
 <div class="container">
+  <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
+  <div class='about-page-video embed-container'>
+    <iframe src='https://player.vimeo.com/video/214095591' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+  </div>
+
   <div class="underline-thin blue">
     <h3 class="secondary-section-title">Brand Ambassador For:</h3>
   </div>
